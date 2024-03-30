a0dc8e33c9df578c5b5a6687b415def016bb7eaa
thrikeswar <119576222+thrikesh@users.noreply.github.com>
2024-03-30 11:49:17 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,26 @@
-﻿
 using System;
 public class Pattern
-{
-    public static void Main()
+ {
+
+    public static void Main() 
     {
-        // type the code here
+        int rows, c = 1, s, i, j; 
+        rows=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Pascal's triangle");
+        for(i = 0; i<rows; i++) 
+        {
+            for(s = 1; s <= rows-i; s++)
+            Console.Write(" ");
+            for(j = 0; j <= i; j++)
+            {
+               if (j == 0||i == 0)
+               c = 1;
+               else
+               c = c*(i-j+1)/j;
+               Console.Write(c + " ");
+            }
+            Console.WriteLine();
+        }
     }
+   
 }
